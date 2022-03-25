2ffcdf149f9f65d0e74238f73c0d5573bc9aee2b
redahm <hammada.reda.dev@gmail.com>
2022-03-25 13:33:01 +0100

    Database connected

---
+++ prototypes/prototype2/products.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class product {
+
+ 
+ private $id;
+ private $productName;
+ private $details;
+ private $prix;
+ private $quantity;
+
+
+}
+
+?>
---
+++ prototypes/prototype2/productsManager.php
@@ -0,0 +1,32 @@
+<?php
+
+class productManager {
+
+    private function  connectDB(){
+
+        $connect = null; 
+        
+        if($connect == null){
+
+          $connect =  mysqli_connect('localhost','Reda','123456','ecommercemakeup');
+
+        }
+
+        else {
+
+                $message = 'database connection error';
+
+                throw new Exception($message);
+        }
+
+            return $connect;
+    }
+}
+
+
+
+
+
+
+
+?>
