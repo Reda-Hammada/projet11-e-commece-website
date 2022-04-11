e556b60971445482dbdbc4d44fd1a481e1622dae
reda hammada <hammada.reda.dev@gmail.com>
2022-04-11 21:59:15 +0000

    merge

--- prototypes/prototype2/productDetails.php
+++ prototypes/prototype2/productDetails.php
@@ -47,7 +47,7 @@ $details = new product();
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>R
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
