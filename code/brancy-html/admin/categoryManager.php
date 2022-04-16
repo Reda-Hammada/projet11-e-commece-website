9f0d3ec2a369fb124d581fcc3cf6dfb51ce6c69a
reda hammada <hammada.reda.dev@gmail.com>
2022-04-16 01:22:31 +0000

    displayCategory method added

--- code/brancy-html/admin/categoryManager.php
+++ code/brancy-html/admin/categoryManager.php
@@ -5,7 +5,7 @@ require 'categoryClass.php';
 
 class categoryManager {
 
-
+// insert categories into database 
     public function addCategory($categoryAdd){ 
     
     $configDatabase = new dataBase();
@@ -15,6 +15,33 @@ class categoryManager {
     
     $insertCategory = "INSERT INTO category (categoryName) VALUES ('$category')";
     $query = mysqli_query($configDatabase->connectDataBase(), $insertCategory);
+
+    }
+
+    // fetch categories to display them on the admin dashboard
+    public function displayCategory(){
+
+        $configDatabase = new dataBase();
+        $configDatabase->connectDataBase();
+        $fetchCategories = "SELECT * FROM category";
+        $query = mysqli_query($configDatabase->connectDataBase(), $fetchCategories);
+        $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+        $categoryFetch =  new category();
+
+        $categoriesArray = array();
+
+        foreach($result  as $categories){
+
+            $categoryFetch->setId($categories['id']);
+            $categoryFetch->setCategoryName($categories['categoryName']);
+
+
+        }
+
+        array_push($categoriesArray, $categoryFetch);
+
+        return $categoriesArray;
+
 
     }
 
