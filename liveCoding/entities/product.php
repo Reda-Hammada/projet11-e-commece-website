8a210ccb7bf5eb7143e6c0a631302e583e95dbae
redahm <hammada.reda.dev@gmail.com>
2022-04-25 11:21:22 +0000

    product class added

--- liveCoding/entities/product.php
+++ liveCoding/entities/product.php
@@ -1,4 +1,63 @@
+<?php 
+
+
+class product {
+
+    private $id;
+    private $productName;
+    private $price;
+    private $details;
+
+
+    public function setId($id){
+
+        $this->id = $id;
+    }
+
+    public function getId(){
+        
+        return $this->id;
+
+    }
+
+    public function setProductName($productName){
+
+        $this->productName = $productName;
+
+    }
+
+    public function getProductName(){
+
+        return $this->productName;
+    }
+
+    public function setPrice($price){
+
+        $this->price = $price;
+    }
+
+    public function getPrice(){
+
+        return $this->price;
+    }
+
+    public function setDetails($details){
+
+        $this->details = $details;
+    }
+
+    public function getDetails(){
+
+        return $this->details;
+    }
+}
 
 
 
 
+
+
+
+
+
+?>
